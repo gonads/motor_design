d3e8fa9e26989c80b4b87ddcc04254be5198ad98
gonads <ne266@cam.ac.uk>
2017-06-24 16:49:58 +0100

    Add files via upload

--- motor_design_ngabbani_24062017.m
+++ motor_design_ngabbani_24062017.m
@@ -1,6 +1,4 @@
-
 %Written by Nadeem Gabbani - 24/06/2017
-
 clear all; close all; clc;
 
 %Set constants and conditions.
@@ -27,7 +25,7 @@ V = (pi*(Ro^2)*L)-(pi*(Ri^2)*L); %Fuel core volume
 M_F = Rho_F*V; %Fuel mass, (kg).
 Rt = 0.0045/2; %Throat radius, (m).
 At = pi*(Rt^2); %Throat area, (m^2).
-Re = 0.00497/2; %Exit radius, (m).
+Re = 0.0045/2; %Exit radius, (m).
 Ae = pi*(Re^2); %Exit area, (m^2).
 ARact = Ae/At; %Exit area to throat area ratio.
 
@@ -41,7 +39,6 @@ Te(1) = T0; %Chamber temperature to atmospheric, (K).
 L(1) = L; %Initial length of fuel cure, (m).
 Ri(1) = Ri; %Initial burn radius, (m).
 V(1) = pi*(Ri(1)^2)*L(1); %Initial burn volume, (m^3).
-
 N(1) = (P(1)*V(1))/(R_F*T(1)); %Initial number of moles.
 Ve(1) = 0; %Initial exhaust velocity, (m/s^2).
 Rho_G(1) = N(1)/V(1); %Initial gas density, (kg/m^3).
@@ -69,33 +66,33 @@ while Ri(i+1) <= Ro; %Combustion can occur if fuel is remaining.
      Pt(i) = P(i)*(1+(G_F-1/2)^(-G_F/(G_F-1))); %Dynamic throat pressure.
      Pe(i) = Pt(i)*((1+(G_F-1/2)*Me^2).^(-G_F/(G_F-1))); %Dynamic exit pressure.
      
-     Rb(i) = (a/1000)*(P(i))^n; %Dynamic burn rate.
-     Ab(i) = 2*pi*Ri(i)*L(i); %Dynamic burn area.
+         Rb(i) = (a/1000)*(P(i))^n; %Dynamic burn rate.
+         Ab(i) = 2*pi*Ri(i)*L(i); %Dynamic burn area.
      
-     mdot1(i) = Ab(i)*Rho_F*Rb(i); %Mass flow rate through chamber.
-     mdot3(i) = ((At*P(i))/(sqrt(T_F)))*sqrt(G_F/R_F)*((G_F+1)/2)^(-(G_F+1)/(2*(G_F-1))); %Mass flow rate through nozzle.
-     mdot2(i) = mdot1(i) - mdot3(i); %Mass flow rate causing back pressure.
+             mdot1(i) = Ab(i)*Rho_F*Rb(i); %Mass flow rate through chamber.
+             mdot3(i) = ((At*P(i))/(sqrt(T_F)))*sqrt(G_F/R_F)*((G_F+1)/2)^(-(G_F+1)/(2*(G_F-1))); %Mass flow rate through nozzle.
+             mdot2(i) = mdot1(i) - mdot3(i); %Mass flow rate causing back pressure.
     
-     T(i) = (P(i)*V(i))/(N(i)*R_F); %Combustion temperature.
-     Tt(i) = T(i)/(1+(G_F-1/2)); %Throat temperature.
-     Te(i) = Tt(i)*((1+(G_F-1/2)*Me^2)^-1); %Exit temperature.
+                 T(i) = (P(i)*V(i))/(N(i)*R_F); %Combustion temperature.
+                 Tt(i) = T(i)/(1+(G_F-1/2)); %Throat temperature.
+                 Te(i) = Tt(i)*((1+(G_F-1/2)*Me^2)^-1); %Exit temperature.
      
-     Rho_G(i) = mdot1(i)/V(i); %Density of gas during combustion, (kg/m^3).          
+                    Rho_G(i) = mdot1(i)/V(i); %Density of gas during combustion, (kg/m^3).          
    
-     Ve(i) = sqrt(((2*G_F)/(G_F-1))*R_F*T(i)*((1-((Pe(i)/P(i))^(G_F-1/G_F))))); %Exhaust velocity.
-     F(i) = (mdot3(i)*Ve(i))+((Pe(i)-P0)*Ae); %Dynamic thrust produced.
-     ISP(i) = F(i)/(mdot3(i)*G0); %Dynamic specific impulse,.
-     Cs (i) = (At*P(i))/mdot3(i); %Characteristic exhaust velocity.
+                         Ve(i) = sqrt(((2*G_F)/(G_F-1))*R_F*T(i)*((1-((Pe(i)/P(i))^(G_F-1/G_F))))); %Exhaust velocity.
+                         F(i) = (mdot3(i)*Ve(i))+((Pe(i)-P0)*Ae); %Dynamic thrust produced.
+                         ISP(i) = F(i)/(mdot3(i)*G0); %Dynamic specific impulse,.
+                         Cs (i) = (At*P(i))/mdot3(i); %Characteristic exhaust velocity.
  
- if i >= 2
-    dP(i)=(P(i)-P(i-1))/dt; %Pressure change.
- else
- end
+    if i >= 2
+         dP(i)=(P(i)-P(i-1))/dt; %Pressure change.
+        else
+    end
  
- N(i+1) = N(i)+(mdot2(i)*dt);
- Ri(i+1) = Ri(i)+(Rb(i)*dt);
- L(i+1) = L(i)-(Rb(i)*dt);
- t(i+1) = t(i)+dt;
+             N(i+1) = N(i)+(mdot2(i)*dt);
+             Ri(i+1) = Ri(i)+(Rb(i)*dt);
+             L(i+1) = L(i)-(Rb(i)*dt);
+             t(i+1) = t(i)+dt;
  
 end
 
@@ -110,23 +107,23 @@ while P(i) >= P(1) %Tail down occurs when fuel runs out.
      Pt(i) = P(i)*(1+(G_F-1/2)^(-G_F/(G_F-1)));
      Pe(i) = Pt(i)*((1+(G_F-1/2)*Me^2)^(-G_F/(G_F-1))); %Exit pressure, (Pa).
      
-     mdot1(i) = 0; %Mass flow rate of fuel, (kg/s).
-     mdot3(i) = ((At*P(i))/(sqrt(T_F)))*sqrt(G_F/R_F)*((G_F+1)/2)^(-(G_F+1)/(2*(G_F-1))); %Mass flow rate through nozzle, (kg/s).
-     mdot2(i) = mdot1(i) - mdot3(i); %Mass flow rate through chamber, (kg/s).
+         mdot1(i) = 0; %Mass flow rate of fuel, (kg/s).
+         mdot3(i) = ((At*P(i))/(sqrt(T_F)))*sqrt(G_F/R_F)*((G_F+1)/2)^(-(G_F+1)/(2*(G_F-1))); %Mass flow rate through nozzle, (kg/s).
+         mdot2(i) = mdot1(i) - mdot3(i); %Mass flow rate through chamber, (kg/s).
     
-     T(i) = (P(i)*V(i))/(N(i)*R_F);
-     Tt(i) = T(i)/(1+(G_F-1/2));
-     Te(i) = Tt(i)*((1+(G_F-1/2)*Me^2)^-1);
+             T(i) = (P(i)*V(i))/(N(i)*R_F);
+             Tt(i) = T(i)/(1+(G_F-1/2));
+             Te(i) = Tt(i)*((1+(G_F-1/2)*Me^2)^-1);
      
-     Rho_G(i) = mdot1(i)/V(i); %Density of gas during combustion, (kg/m^3).          
+                Rho_G(i) = mdot1(i)/V(i); %Density of gas during combustion, (kg/m^3).          
    
-     Ve(i) = sqrt(((2*G_F)/(G_F-1))*R_F*T(i)*((1-((Pe(i)/P(i))^(G_F-1/G_F)))));
-     F(i) = (mdot3(i)*Ve(i))+((Pe(i)-P0)*Ae); %Thrust produced, (N).
-     ISP(i) = F(i)/(mdot3(i)*G0); %Resulant specific impulse, (s).
-     Cs (i) = (At*P(i))/mdot3(i);
+                     Ve(i) = sqrt(((2*G_F)/(G_F-1))*R_F*T(i)*((1-((Pe(i)/P(i))^(G_F-1/G_F)))));
+                     F(i) = (mdot3(i)*Ve(i))+((Pe(i)-P0)*Ae); %Thrust produced, (N).
+                     ISP(i) = F(i)/(mdot3(i)*G0); %Resulant specific impulse, (s).
+                     Cs (i) = (At*P(i))/mdot3(i);
 
-    N(i+1) = N(i)+(mdot2(i)*dt);
-    t(i+1) = t(i)+dt;
+                        N(i+1) = N(i)+(mdot2(i)*dt);
+                        t(i+1) = t(i)+dt;
   
 end
 
