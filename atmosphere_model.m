6038780f117ea810c274b2833bf20748067a031a
gonads <ne266@cam.ac.uk>
2017-06-28 23:08:37 +0100

    Add files via upload
    Have now turned it into a function. Still outputs the graphs when ran.

--- atmosphere_model.m
+++ atmosphere_model.m
@@ -1,12 +1,12 @@
 % Written by Nadeem Gabbani - 28/06/2017
 
-clear all; close all; clc;
+function [Ta, p, rho, a] = atmosphere_model(H)
 
 G0 = 6.67408E-11; % Gravitational constant, (m^3/kg.s^2).
 M0 = 5.972E24; % Mass of Earth, (kg).
 R0 = 6371E3; % Radius of Earth, (m).
 
-T = []; % Set initial temperature to zero.
+Ta = []; % Set initial temperature to zero.
 p = []; % Set initial pressure to zero.
 a = []; % Set initial gravitation acceleration to zero.
 k = 0; % Start at step zero.
@@ -15,21 +15,21 @@ for H = 0:100000
    k = k + 1;
   
 if H < 11000
-    T(k) = 273.1+(15.04-(0.00649*H));
-    p(k) = 101.29*(((T(k))/288.08).^5.256);
-    rho(k) = p(k)/(0.2869*(T(k)));  
+    Ta(k) = 273.1+(15.04-(0.00649*H));
+    p(k) = 101.29*(((Ta(k))/288.08).^5.256);
+    rho(k) = p(k)/(0.2869*(Ta(k)));  
     a(k) = (G0*M0)/(R0+H)^2;
     
 elseif H < 25000 
-    T(k) = 273.1-56.46;
+    Ta(k) = 273.1-56.46;
     p(k) = 22.65*exp(1.73-0.000157*H);
-    rho(k) = p(k)/(0.2869*(T(k)));  
+    rho(k) = p(k)/(0.2869*(Ta(k)));  
     a(k) = (G0*M0)/(R0+H)^2;
     
 else
-    T(k) = 273.1-131.21+(0.00299*H);
-    p(k) = 2.488*(((T(k))/216.6)^-11.388); 
-    rho(k) = p(k)/(0.2869*(T(k)));
+    Ta(k) = 273.1-131.21+(0.00299*H);
+    p(k) = 2.488*(((Ta(k))/216.6)^-11.388); 
+    rho(k) = p(k)/(0.2869*(Ta(k)));
     a(k) = (G0*M0)/(R0+H)^2;
     
     end
@@ -38,7 +38,7 @@ end
 H = 0:100000;
 
 figure;
-plot(H, T); 
+plot(H, Ta); 
 title('Temperature vs. Altitude');
 xlabel('Altitude (m)');
 ylabel('Temperature (K)');
@@ -69,3 +69,4 @@ ylabel('Gravitational Acceleration (m/s^2)');
 set(gca,'TickDir','out');
 grid minor;
 
+end
